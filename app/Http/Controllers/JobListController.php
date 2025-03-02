76f87cd2890cb80b0f498cf345b0f04194c4cc46
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-03-02 19:07:50 +0530

    Pass data

--- app/Http/Controllers/JobListController.php
+++ app/Http/Controllers/JobListController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Company;
+use App\Models\JobType;
 use Illuminate\Http\Request;
 
 class JobListController extends Controller
 {
-    //
+    public function index()
+    {
+        $companies = Company::get();
+        $job_types = JobType::select('id', 'name')->get();
+        $categories = Category::select('id', 'name')->get();
+
+        $data = [
+            'companies' => $companies,
+            'categories' => $categories,
+            'job_types' => $job_types,
+        ];
+
+        return view('front.jobs.index', compact('data'));
+    }
 }
