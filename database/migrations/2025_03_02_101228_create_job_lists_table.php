47079dd1d024f1fc3617f5cefb99bb3249fc981d
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-03-02 16:04:27 +0530

    Create a schema, model, and controller for Job lists, job type, job category, and company

---
+++ database/migrations/2025_03_02_101228_create_job_lists_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->boolean("status")->default(1);
+            $table->timestamps();
+        });
+
+        Schema::create('job_types', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->boolean("status")->default(1);
+            $table->timestamps();
+        });
+
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('location')->nullable();
+            $table->string('website')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('job_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('job_type_id')->constrained()->onDelete('cascade');
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
+            $table->integer('vacancy');
+            $table->string('salary')->nullable();
+            $table->string('location')->nullable();
+            $table->longText('description');
+            $table->mediumText('benefits')->nullable();
+            $table->mediumText('responsiblity')->nullable();
+            $table->mediumText('qualifications')->nullable();
+            $table->mediumText('keywords')->nullable();
+            $table->integer('experience');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_lists');
+        Schema::dropIfExists('companies');
+        Schema::dropIfExists('job_types');
+        Schema::dropIfExists('categories');
+    }
+};
