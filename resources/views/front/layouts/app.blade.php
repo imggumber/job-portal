e2a55cff7829bc5e1fadb34c848017df5240890a
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-03-02 19:07:11 +0530

    Call modals

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -38,7 +38,7 @@
 					@else
 					<a class="btn btn-outline-primary me-2" href="{{ route('account.profile') }}" type="submit">Profile</a>
 					@endif
-					<a class="btn btn-primary" href="post-job.html" type="submit">Post a Job</a>
+					<a class="btn btn-primary" href="{{ route('job.job') }}" type="submit">Post a Job</a>
 				</div>
 			</div>
 		</nav>
@@ -46,30 +46,11 @@
 
 	@yield('main')
 
-	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title pb-0" id="exampleModalLabel">Change Profile Picture</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
-				<div class="modal-body">
-					<form method="post" id="proficPicForm" name="proficPicForm" class="form proficPicForm">
-						<div class="mb-3">
-							<label for="exampleInputEmail1" class="form-label">Profile Image</label>
-							<input type="file" class="form-control" id="image" name="image">
-							<p></p>
-						</div>
-						<div class="d-flex justify-content-end">
-							<button type="submit" class="btn btn-primary mx-3">Update</button>
-							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-						</div>
+	<!-- Add profile picture model -->
+	@include('front.partials.modals.add-profile')
 
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+	<!-- Add company modal -->
+	@include('front.partials.modals.add-company')
 
 	<footer class="bg-dark py-3 bg-2">
 		<div class="container">
