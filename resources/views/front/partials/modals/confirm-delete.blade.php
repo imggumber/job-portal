92a58bf201ea65be3cf103dcdba89471aa2b45ce
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-03-22 21:31:39 +0530

    Create modal to show confirmation message

---
+++ resources/views/front/partials/modals/confirm-delete.blade.php
@@ -0,0 +1,19 @@
+<div id="confirm-delete-modal" class="position-absolute top-50 start-50 translate-middle">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5 pb-0" id="staticBackdropLabel">Are you sure you want to delete?</h1>
+            </div>
+            <div class="modal-body">
+                <p class="mb-0">
+                    This action is permanent and cannot be undone. Please make sure that you really want to remove this before proceeding.
+                </p>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary close-del-modal" data-bs-dismiss="modal">Close</button>
+                <button type="button" id="del-job-btn" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
